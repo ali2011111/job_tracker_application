7b05392d813ca2b48e53df7654799b849a9f1757
Mohammad Ali Shakil <mohas@ulrik.uio.no>
2026-02-26 08:55:49 +0100

    feat: la til handleDelete og handleStatusChange

--- src/components/ApplicationList.jsx
+++ src/components/ApplicationList.jsx
@@ -1,4 +1,19 @@
+import { STATUS } from '../constants/status'
+
 function ApplicationList({ applications, setApplications }) {
+    
+    const handleDelete = (id) => {
+        const updated = applications.filter(app => app.id !== id)
+        setApplications(updated)
+    }
+
+    const handleStatusChange = (id, newStatus) => {
+        const updated = applications.map(app => 
+            app.id === id ? { ...app, status: newStatus } : app
+        )
+        setApplications(updated)
+     }
+
   if (applications.length === 0) {
     return <p>No applications added yet.</p>
   }
@@ -10,11 +25,11 @@ function ApplicationList({ applications, setApplications }) {
 
   return (
     <div style={{ marginTop: "2rem" }}>
-      <h2>Application List</h2>
+      <h2>Applications</h2>
 
       {applications.map((app) => (
-        <div 
-          key={app.id} 
+        <div
+          key={app.id}
           style={{
             border: "1px solid black",
             padding: "1rem",
@@ -23,14 +38,35 @@ function ApplicationList({ applications, setApplications }) {
         >
           <h3>{app.company}</h3>
           <p>Position: {app.position}</p>
-          <p>Status: {app.status}</p>
+
+          <div>
+            <label>Status: </label>
+            <select
+              value={app.status}
+              onChange={(e) =>
+                handleStatusChange(app.id, e.target.value)
+              }
+            >
+              {Object.values(STATUS).map(statusOption => (
+                <option key={statusOption} value={statusOption}>
+                  {statusOption}
+                </option>
+              ))}
+            </select>
+          </div>
+
           <p>Deadline: {app.deadline || "No deadline"}</p>
+
+          <button
+            style={{ marginTop: "0.5rem" }}
+            onClick={() => handleDelete(app.id)}
+          >
+            Delete
+          </button>
         </div>
       ))}
-
     </div>
   )
-      
 }
 
 export default ApplicationList
